e38ae7e0dd2bb89c81b7f5973e6ca913b86be8a2
Paulo Moreira <paulomoreira.iblue@ciclic.com.br>
2020-05-14 18:32:47 -0300

    Alteração de nome

--- src/main/java/br/com/paulomoreira/starB2W/StarB2WApplication.java
+++ src/main/java/br/com/paulomoreira/starB2W/StarB2WApplication.java
@@ -1,13 +1,13 @@
-package br.com.paulomoreira.startB2W;
+package br.com.paulomoreira.starB2W;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StartB2WApplication {
+public class StarB2WApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StartB2WApplication.class, args);
+		SpringApplication.run(StarB2WApplication.class, args);
 	}
 
 }
